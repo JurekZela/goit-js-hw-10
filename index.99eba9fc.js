782e5b88bac6edafce5e3e83df5eff5ca2bebf1f
JurekZela <122160360+JurekZela@users.noreply.github.com>
2023-09-23 20:14:32 +0000

    Deploying to gh-pages from @ JurekZela/goit-js-hw-10@e6588d53071e14f0175a4e9f50f1401b6de05b6c 🚀

--- index.99eba9fc.js
+++
@@ -1,2 +0,0 @@
-fetch("https://jsonplaceholder.typicode.com/users").then((e=>{})).then((e=>{})).catch((e=>{}));
-//# sourceMappingURL=index.99eba9fc.js.map
--- index.a2fc9699.js
+++
@@ -1,2 +0,0 @@
-fetch("https://jsonplaceholder.typicode.com/users").then((function(t){})).then((function(t){})).catch((function(t){}));
-//# sourceMappingURL=index.a2fc9699.js.map
