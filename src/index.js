a099a3c3ee8e9290403da96aaee3ef0c4956b072
JurekZela <122160360+JurekZela@users.noreply.github.com>
2023-09-24 19:38:45 +0200

    timeout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import SlimSelect from 'slim-select'
 import 'slim-select/dist/slimselect.css';
+
 import { fetchBreeds, fetchCatByBreed } from "./cat-api";
 import '../src/styles.css';
+
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refBreedSelect = document.querySelector('.breed-select')
@@ -15,13 +17,13 @@ refBreedSelect.addEventListener('change', onCreateCard);
 function onCreateCard(e) {
   const targetSelectValue = e.target.value;
   refLoader.style.display = "block";
- 
+  
   return fetchCatByBreed(targetSelectValue)
   .then(cat => {
-   
+    
     const markup = createCardBreeds(cat);
     refCatInfo.innerHTML = '';
-
+    
     if (!markup.textContent) {
       refCatInfo.insertAdjacentHTML("beforeend",  markup);
       refLoader.style.display = "none";
@@ -35,16 +37,17 @@ function onCreateCard(e) {
 function onError() {
   refLoader.style.display = "none";
   refCatInfo.innerHTML = '';
-
-  Notify.failure('Oops! Something went wrong! Try reloading the page or select another cat breed!',  {
-    position: 'left-bottom',
-    width: '400px',
-  });
+  
+  Notify.failure('Oops! Something went wrong! Try reloading the page or select another cat breed!',
+  {position: 'left-bottom',
+  width: '400px',
+  timeout: 3000,
+});
 };
 
 
 function createCardBreeds(data) {  
-return data.map(({ url, breeds:{ [0]:{name}, [0]:{temperament}, [0]:{description}} }) => {
+return data.map(({ url, breeds:{ [0]:{ name }, [0]:{ temperament }, [0]:{ description }} }) => {
   return `<li class = "card">
   <img class="image" src="${url}" alt="${name}"/>
   <div class = "block-cat-info">
